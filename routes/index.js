6869ba7d0fad8458105eebf02c4b63899c213bd0
M1123 <dbv93@yandex.ru>
2019-06-28 06:36:12 +0700

    getData

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,27 @@
 var express = require('express');
 var router = express.Router();
 
+
+var weather = require('weather-js');
+ 
+// Options:
+// search:     location name or zipcode
+// degreeType: F or C
+var pog;
+weather.find({search: 'Novosibirsk, RU', degreeType: 'c'}, function(err, result) {
+  if(err) console.log(err);
+  // console.log(JSON.stringify(result, null, 2));
+  pog =result;
+  
+  // console.log(pog[1]);
+});
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-router.get('/1', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.use('/1', function(req, res, next) {
+  res.render('weather', { title: pog[1].location.name, d:pog[1]});
 });
 
 module.exports = router;
